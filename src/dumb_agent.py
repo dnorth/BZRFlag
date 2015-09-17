ca229844a8ca3e5132c1b06570275887ff9eac8e
Danny North <dnorth2@hotmail.com>
2015-09-16 22:00:57 -0600

    Created a very dumb agent that just spins in 60 degree circles and shoots

--- src/dumb_agent.py
+++ src/dumb_agent.py
@@ -1,18 +1,33 @@
-import sys, argparse
+import sys, argparse, time
+from bzrc import BZRC, Command
 
 # --------------------------------- VARIABLES
 desc=''' Example:
     python dumb_agent.py -p localhost -s 57413
     '''
+# --------------------------------- Agent Object
+class Agent(object):
+    def __init__(self, bzrc):
+        self.bzrc = bzrc
+        self.constants = self.bzrc.get_constants()
+        self.commands = []
+        self.time_diff = 0
+
+    def tick(self, time_diff):
+        myTanks = self.bzrc.get_mytanks()
+        print "Time difference: " + str(time_diff)
+        for tank in myTanks:
+            #Command takes 4 Parameters- (Tank Index, Speed, Angular Velocity, Whether or not to be shooting)
+            command = Command(tank.index, 1, 60, True)
+            self.commands.append(command)
+
+        results = self.bzrc.do_commands(self.commands)
 
 # --------------------------------- FUNCTIONS
 def startRobot(hostname, socket):
-	
-
-
-
-
-
+    bzrc = BZRC(hostname, socket)
+    agent = Agent(bzrc)
+    return bzrc, agent
 
 # --------------------------------- MAIN FUNCTION
 def readCommandLine():
@@ -31,4 +46,16 @@ if __name__ == '__main__':
     hostname = args.host
     socket = int(args.socket)
 
-    print hostname, socket
+    bzrc, agent = startRobot(hostname, socket)
+
+    prev_time = time.time()
+
+
+    # Run the agent
+    try:
+        while True:
+            time_diff = time.time() - prev_time
+            agent.tick(time_diff)
+    except KeyboardInterrupt:
+        print "Exiting due to keyboard interrupt."
+        bzrc.close()
