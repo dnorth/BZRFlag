3ea3e7948cca39ebb4fe3ca03e1c97ebd1455324
tgrossen <tgrossen@gmail.com>
2015-09-21 00:14:54 -0600

    Avoidance fields kinda working...?

--- src/pf_agent.py
+++ src/pf_agent.py
@@ -21,8 +21,11 @@ class Agent(object):
         self.shoot = False
         self.bases = self.getBases()
         self.color = self.getMyColor()
-        self.s = 5
-        self.alpha = 5
+        self.s = 10
+        self.alpha, self.beta = 5, 10
+        self.s_obstacles = self.getObstacles()
+        self.d_obstacles = {}
+        self.move = {}
 
     def getMyColor(self):
         return self.tanks[0].callsign.split('0')[0]
@@ -32,6 +35,37 @@ class Agent(object):
 
     def calcCenter(self, base):
         return (base.corner1_x + base.corner3_x) / 2 , (base.corner1_y + base.corner3_y) / 2
+
+    def getObstacles(self):
+        n = 0
+        obstacles = {}
+        for obstacle in self.bzrc.get_obstacles():
+            obstacles[n] = {}
+            obstacles[n]['x'] = (obstacle[0][0] + obstacle[2][0]) / 2
+            obstacles[n]['y'] = (obstacle[0][1] + obstacle[2][1]) / 2
+            obstacles[n]['r'] = self.getDistance(obstacles[n]['x'], obstacles[n]['y'], obstacle[0][0], obstacle[0][1]) * 1.1
+            n +=1
+        print obstacles
+        return obstacles
+
+        # n = 0
+        # obstacles = {}
+        # for obstacle in self.bzrc.get_obstacles():
+        #     for x in range(len(obstacle)):
+        #         obstacles[n] = {}
+        #         obstacles[n]['x'] = obstacle[x][0]
+        #         obstacles[n]['y'] = obstacle[x][1]
+        #         next = x+1
+        #         if next >= len(obstacle):
+        #             next = 0
+        #         dist = self.getDistance(obstacle[x][0], obstacle[x][1], obstacle[next][0], obstacle[next][1])
+        #         obstacles[n]['r'] = dist * 0.25
+        #         n += 1
+        # # print obstacles
+        # return obstacles
+
+    def getTankFromCallsign(self, callsign):
+        return filter(lambda x : x.callsign == callsign, self.tanks)[0]
 
     def getBases(self):
         bases = {}
@@ -56,29 +90,32 @@ class Agent(object):
         relative_angle = self.normalize_angle(target_angle - tank.angle)
         self.bzrc.do_commands([Command(tank.index, 1, 2 * relative_angle, True)])
     
-    def getGoalDistance(self, tank, x, y):
+    def getDistance(self, x_1, y_1, x_2, y_2):
+        x_diff = x_1 - x_2
+        y_diff = y_1 - y_2
+        return pow(pow(x_diff, 2) + pow(y_diff, 2), 0.5)
+
+    def getTankDistance(self, tank, x, y):
         x_diff = x - tank.x
         y_diff = tank.y - y
         return pow(pow(x_diff, 2) + pow(y_diff, 2), 0.5)
 
-    def setGoalAngles(self):
-        for tank in self.tanks:
-            y = self.goals[tank.callsign]['y']
-            x = self.goals[tank.callsign]['x']
-            self.goals[tank.callsign]['angle'] = atan2(y - tank.y, x - tank.x)
+    def getTankAngle(self, tank, x, y):
+        return atan2(y - tank.y, x - tank.x)
 
-    def setGoalCoords(self):
+    def setGoalInfo(self):
         for tank in filter(lambda x : x.status == 'alive', self.tanks):
-            if tank not in self.goals:
+            if tank.callsign not in self.goals:
                 self.goals[tank.callsign] = {}
             t = self.goals[tank.callsign]
             if tank.flag != "-":
                 t['x'], t['y'] = self.bases[self.color].center
                 t['r'] = 15
-                t['distance'] = self.getGoalDistance(tank, t['x'], t['y'])
+                t['distance'] = self.getTankDistance(tank, t['x'], t['y'])
             else:
                 t['x'], t['y'], t['distance'] = self.findClosestFlag(tank)
                 t['r'] = 1
+            t['angle'] = self.getTankAngle(tank, t['x'], t['y'])
 
     def findClosestFlag(self, tank):
         closestFlag = None
@@ -86,18 +123,19 @@ class Agent(object):
         for flag in filter(lambda x: x.poss_color != self.color, self.flags):
             if flag.color == self.color:
                 continue
-            dist = self.getGoalDistance(tank, flag.x, flag.y)
+            dist = self.getTankDistance(tank, flag.x, flag.y)
             if dist < closestDistance:
                 closestFlag = flag
                 closestDistance = dist
+        if not closestFlag:
+            return 0, 0, self.getTankDistance(tank, 0, 0)
         return closestFlag.x, closestFlag.y, closestDistance
 
     def seekGoals(self):
         for tank in self.tanks:
             if tank.status == 'alive':
                 if tank.callsign in self.goals:
-                    x_diff, y_diff = self.seekGoal(tank)
-                    self.moveToPosition(tank, tank.x+x_diff, tank.y+y_diff)
+                    self.move[tank.callsign] = list(self.seekGoal(tank))
 
     def seekGoal(self, tank):
         x_diff = 0
@@ -106,12 +144,53 @@ class Agent(object):
         if g['distance'] < g['r']:
             pass
         elif g['r'] <= g['distance'] and g['distance'] <= (self.s + g['r']):
-            x_diff = self.aplha * (g['distance'] - g['r']) * cos(g['angle'])
-            y_diff = self.aplha * (g['distance'] - g['r']) * sin(g['angle'])
+            x_diff = self.alpha * (g['distance'] - g['r']) * cos(g['angle'])
+            y_diff = self.alpha * (g['distance'] - g['r']) * sin(g['angle'])
         elif g['distance'] > self.s + g['r']:
             x_diff = self.alpha * self.s * cos(g['angle'])
             y_diff = self.alpha * self.s * sin(g['angle'])
         return x_diff, y_diff
+
+    def setObstacleInfo(self):
+        for tank in filter(lambda x : x.status == 'alive', self.tanks):
+            if tank.callsign not in self.d_obstacles:
+                self.d_obstacles[tank.callsign] = {}
+            t = self.d_obstacles[tank.callsign]
+            for obstacle in self.s_obstacles:
+                t[obstacle] = {}
+                t[obstacle]['distance'] = self.getTankDistance(tank, self.s_obstacles[obstacle]['x'], self.s_obstacles[obstacle]['y'])
+                t[obstacle]['angle'] = self.getTankAngle(tank, self.s_obstacles[obstacle]['x'], self.s_obstacles[obstacle]['y'])
+                t[obstacle]['r'] = self.s_obstacles[obstacle]['r']
+
+    def avoidObstacles(self):
+        for tank in self.tanks:
+            if tank.status == 'alive':
+                if tank.callsign in self.d_obstacles:
+                    x_diff, y_diff = self.avoidObstacle(tank)
+                    # print "tick"
+                    # print x_diff, y_diff
+                    # print self.move[tank.callsign][0], self.move[tank.callsign][1]
+                    self.move[tank.callsign][0] += x_diff
+                    self.move[tank.callsign][1] += y_diff
+
+    def avoidObstacle(self, tank):
+        x_diff = 0
+        y_diff = 0
+        for obstacle, o in self.d_obstacles[tank.callsign].items():
+            if o['distance'] < o['r']:
+                x_diff += -1 * cos(o['angle']) * (5000 / o['distance'])
+                y_diff += -1 * sin(o['angle']) * (5000 / o['distance'])
+            elif o['r'] <= o['distance'] and o['distance'] <= self.s + o['r']:
+                x_diff += -1 * self.beta * (self.s + o['r'] - o['distance']) * cos(o['angle'])
+                y_diff += -1 * self.beta * (self.s + o['r'] - o['distance']) * sin(o['angle'])
+            elif o['distance'] > self.s + o['r']:
+                continue
+        return x_diff, y_diff
+
+    def moveTanks(self):
+        for callsign, m in self.move.items():
+            tank = self.getTankFromCallsign(callsign)
+            self.moveToPosition(tank, tank.x+m[0], tank.y+m[1])
 
 # --------------------------------- FUNCTIONS
 def startRobot(hostname, socket):
@@ -123,14 +202,16 @@ def runTimer(bzrc, agent, log):
     start_time = time.time()
     while True:
         try:
+            print "tick"
             agent.update()
-            agent.setGoalCoords()
-            agent.setGoalAngles()
+            agent.setGoalInfo()
+            agent.setObstacleInfo()
             agent.seekGoals()
+            agent.avoidObstacles()
+            agent.moveTanks()
         except KeyboardInterrupt:
             print "Exiting due to keyboard interrupt."
             agent.stop = True
-            agent.shoot_thread.join()
             bzrc.close()
             return      
 
