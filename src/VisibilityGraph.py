698684c84666e742796ede508e53b69371467659
tgrossen <tgrossen@gmail.com>
2015-10-06 16:21:25 -0600

    Updates to visibilitygraph

--- src/VisibilityGraph.py
+++ src/VisibilityGraph.py
@@ -2,7 +2,29 @@ from bzrc import BZRC, Command
 
 def startRobot(hostname, socket):
     bzrc = BZRC(hostname, socket)
-    agent = Agent(bzrc)
-    return bzrc, agent
+    return bzrc
 
-def VisibilityGraph(s):
+bzrc = startRobot('localhost', 60960)
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class VisibilityLine():
+    def __init__():
+
+def checkIntersection(p1, p2, p3, p4):
+    # proper intersection
+    
+
+def getPoints(step):
+    points = []
+    for x in range(-400, 401, step):
+        for y in range(-400, 401, step):
+            points.append(Point(x, y))
+    return points
+
+def VisibilityGraph(bzrc):
+    points = getPoints(20)
+    s = bzrc.get_obstacles()
