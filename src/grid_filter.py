0497bd0bccb591222f56a924aa8daf9f893f6818
tgrossen <tgrossen@gmail.com>
2015-11-30 00:48:57 -0700

    Noisy, innacurate. Trying to figure out why.

--- src/grid_filter.py
+++ src/grid_filter.py
@@ -10,6 +10,9 @@ from numpy import zeros
 
 WIDTH = 400
 HEIGHT = 400
+THRESHOLD = 0.75
+likelihood = {}
+prior = {1:0.875, 0:0.125}
 
 desc=''' Example:
     python grid_filter.py -p localhost -s 57413
@@ -35,6 +38,33 @@ def draw_grid():
 def update_grid(new_grid):
     global grid
     grid = new_grid
+
+def normalizer(o):
+    return likelihood[o,1]*prior[1] + likelihood[o,0]*prior[0]
+
+def bayes(o, s):
+    l = likelihood[o,s]
+    # print "likelihood is: " + str(l)
+    p = prior[s]
+    # print "prior is: " + str(p)
+    print "THE NUMBER: " + str((l*p)/normalizer(o))
+    if (l*p)/normalizer(o) >= THRESHOLD:
+        # print "1 dude"
+        return 1
+    else:
+        # print "0 face"
+        return 0
+
+def get_bayes_grid(local_grid, position):
+    x = position[0]+WIDTH
+    y = position[1]+HEIGHT
+    x_len = len(local_grid[0])
+    y_len = len(local_grid)
+    spec_grid = grid[y:y+y_len,x:x+x_len]
+    for x in range(0, len(local_grid)):
+        for y in range(0, len(local_grid[0])):
+            spec_grid[x][y] = bayes(local_grid[x][y], spec_grid[x][y])
+    return spec_grid
 
 def update_local_grid(local_grid, position):
     x = position[0]+WIDTH
@@ -65,18 +95,23 @@ class Agent(object):
         self.commands = []
         self.time_diff = 0
         self.stop = False
-        # self.bases = self.getBases()
-        # self.color = self.getMyColor()
-        self.move = {}
+        self.constants = self.bzrc.get_constants()
+        likelihood[1,1] = float(self.constants['truepositive']) #truepositive
+        likelihood[1,0] = 1 - likelihood[1,1] #falsepositive
+        likelihood[0,0] = float(self.constants['truenegative']) #truenegative
+        likelihood[0,1] = 1 - likelihood[0,0] #falsenegative
     def read(self):
         for tank in filter(lambda x : x.status == 'alive' and x.index == 0, self.tanks):
             position, local_grid = self.bzrc.get_occgrid(tank.index)
-            rot_grid = numpy.rot90(local_grid)
-            update_local_grid(rot_grid, position)
+            rot_grid = numpy.rot90(local_grid, 3)
+            bayes_grid = get_bayes_grid(rot_grid, position)
+            update_local_grid(bayes_grid, position)
     def update(self):
         self.tanks = self.bzrc.get_mytanks()
-    # def move(self):
-        # for tank in 
+    def move(self):
+        for tank in filter(lambda x : x.status == 'alive' and x.index == 0, self.tanks):
+            self.bzrc.speed(tank.index, 10)
+            self.bzrc.angvel(tank.index, 0.2)
 
 def runTimer(bzrc, agent, log=False):
     # start_time = time.time()
@@ -85,7 +120,7 @@ def runTimer(bzrc, agent, log=False):
         try:
             agent.update()
             agent.read()
-            # agent.move()
+            agent.move()
             draw_grid()
         except KeyboardInterrupt:
             print "Exiting due to keyboard interrupt."
