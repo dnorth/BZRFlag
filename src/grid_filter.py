e6ace6c5d548f02bb992bf9c724896b85d014573
tgrossen <tgrossen@gmail.com>
2015-11-30 15:53:10 -0700

    Danny says: Just push that for now.

--- src/grid_filter.py
+++ src/grid_filter.py
@@ -11,9 +11,11 @@ from itertools import izip
 
 WIDTH = 400
 HEIGHT = 400
-THRESHOLD = 0.67
+THRESHOLD = 0.9
 likelihood = {}
-prior = {0:0.05, 1:0.95}
+# prior = {1:0.5, 0:0.5}
+prior = numpy.empty([800,800])
+prior.fill(0.8)
 
 desc=''' Example:
     python grid_filter.py -p localhost -s 57413
@@ -36,48 +38,61 @@ def draw_grid():
     glFlush()
     glutSwapBuffers()
 
-def update_grid(new_grid):
+def update_grid():
     global grid
-    grid = new_grid
+    grid = prior
 
-def normalizer(o):
-    return likelihood[o,1]*prior[1] + likelihood[o,0]*prior[0]
+# def normalizer(o, x, y):
+#     if o == 1:
+#         return likelihood['truepositive']*prior[x][y] + likelihood['falsepositive']*(1-prior[x][y])
+#     else:
+#         return likelihood['falsenegative']*prior[x][y] + likelihood['truenegative']*(1-prior[x][y])
 
-def bayes(o, s):
-    l = likelihood[o,s]
+def bayes(o, x, y):
+    # l = likelihood[o,thresh(s)]
     # print "likelihood is: " + str(l)
-    p = prior[s]
-    # print "prior is: " + str(p)
-    if (l*p)/normalizer(o) >= THRESHOLD:
-        #print "THE NUMBER: " + str((l*p)/normalizer(o))
-        return 1
+    if o == 1:
+        bel_occ = likelihood['truepositive'] * prior[x][y]
+        bel_unocc =  likelihood['falsepositive'] * (1-prior[x][y])
     else:
-        # print "0 face"
-        return 0
+        bel_occ = likelihood['falsenegative'] * prior[x][y]
+        bel_unocc =  likelihood['truenegative'] * (1-prior[x][y])
+    # print "THE NUMBER: " + str((l*p)/normalizer(o))
+
+    posterior = (bel_occ)/(bel_occ + bel_unocc)
+    print "posterior is: " + str(posterior)
+
+    # if posterior < 0.001:
+    #     posterior = 0
+
+    prior[x][y] = posterior
+
+    # return prior[x][y]
 
 def get_bayes_grid(local_grid, position):
-    x = position[0]+WIDTH
-    y = position[1]+HEIGHT
-    x_len = len(local_grid[0])
-    y_len = len(local_grid)
-    spec_grid = grid[y:y+y_len,x:x+x_len]
-    # for x in range(0, len(local_grid)):
-    #     for y in range(0, len(local_grid[0])):
-    #         spec_grid[x][y] = bayes(local_grid[x][y], spec_grid[x][y])
-    new_grid = []
-    for a_row, b_row in izip(spec_grid, local_grid):
-        new_row = []
-        for a_item, b_item in izip(a_row, b_row):
-            new_row.append(bayes(a_item, b_item))
-        new_grid.append(new_row)
-    return new_grid
+    x_pos = position[0]+WIDTH
+    y_pos = position[1]+HEIGHT
+    # x_len = len(local_grid[0])
+    # y_len = len(local_grid)
+    # spec_grid = grid[y_pos:y_pos+y_len,x_pos:x_pos+x_len]
+    for x in range(0, len(local_grid)):
+        for y in range(0, len(local_grid[0])):
+            bayes(local_grid[x][y], x+x_pos, y+y_pos)
+    # return spec_grid
+    # new_grid = []
+    # for a_row, b_row in izip(local_grid, spec_grid):
+    #     new_row = []
+    #     for a_item, b_item in izip(a_row, b_row):
+    #         new_row.append(bayes(a_item, b_item))
+    #     new_grid.append(new_row)
+    # return new_grid
 
-def update_local_grid(local_grid, position):
-    x = position[0]+WIDTH
-    y = position[1]+HEIGHT
-    x_len = len(local_grid[0])
-    y_len = len(local_grid)
-    grid[y:y+y_len,x:x+x_len] = local_grid
+# def update_local_grid(local_grid, position):
+#     x = position[0]+WIDTH
+#     y = position[1]+HEIGHT
+#     x_len = len(local_grid[0])
+#     y_len = len(local_grid)
+#     grid[y:y+y_len,x:x+x_len] = local_grid
 
 def init_window(width, height):
     global window
@@ -102,17 +117,17 @@ class Agent(object):
         self.time_diff = 0
         self.stop = False
         self.constants = self.bzrc.get_constants()
-        likelihood[1,1] = float(self.constants['truepositive']) #truepositive
-        likelihood[0,1] = 1 - float(self.constants['truenegative']) #falsenegative
-        likelihood[0,0] = float(self.constants['truenegative']) #truenegative
-        likelihood[1,0] = 1 - float(self.constants['truepositive']) #falsepositive
+        likelihood['truepositive'] = float(self.constants['truepositive']) #truepositive
+        likelihood['falsenegative'] = 1 - float(self.constants['truenegative']) #falsenegative
+        likelihood['truenegative'] = float(self.constants['truenegative']) #truenegative
+        likelihood['falsepositive'] = 1 - float(self.constants['truepositive']) #falsepositive
         self.moving = False
     def read(self):
         for tank in filter(lambda x : x.status == 'alive', self.tanks):
             position, local_grid = self.bzrc.get_occgrid(tank.index)
             rot_grid = numpy.fliplr(numpy.rot90(local_grid, 3))
             bayes_grid = get_bayes_grid(rot_grid, position)
-            update_local_grid(bayes_grid, position)
+            update_grid()
     def update(self):
         self.tanks = self.bzrc.get_mytanks()
     def move(self):
